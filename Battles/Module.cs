0cb78e0fc9dc9e1df5ccc91777ba5437fc2eabcf
marcomeyer <marcomeyer@hispeed.ch>
2012-10-04 22:26:37 +0200

    - step 4: Ninject.Extensions.Conventions

--- Battles/Module.cs
+++ Battles/Module.cs
@@ -1,11 +1,10 @@
 ﻿namespace Battles
 {
-    using Battles.Log;
     using Battles.UI;
 
-    using Ninject.Extensions.Factory;
+    using Ninject.Extensions.Conventions;
     using Ninject.Modules;
-
+    
     public class Module : NinjectModule
     {
         private const int RedArmySize = 20;
@@ -13,25 +12,40 @@
 
         public override void Load()
         {
-            this.Kernel.Bind<ILogService>().To<LogService>().InSingletonScope();
-            
+            this.Kernel.Bind(
+                x =>
+                x.FromThisAssembly().SelectAllClasses().Where(t => t.Name.EndsWith("Service"))
+                    .BindAllInterfaces()
+                    .Configure(b => b.InSingletonScope()));
+
             this.Kernel.Bind<IWeapon>().To<Sword>();
             this.Kernel.Bind<IWarrior>().To<Ninja>();
-            this.Kernel.Bind<IWarriorFactory>().ToFactory();
 
-            this.Kernel.Bind<IFightEngine>().To<FightEngine>();
-            this.Kernel.Bind<IBattleEngine>().To<BattleEngine>()
-                .WithConstructorArgument("redArmySize", RedArmySize)
-                .WithConstructorArgument("blueArmySize", BlueArmySize);
+            this.Kernel.Bind(
+                x => 
+                x.FromThisAssembly().SelectAllInterfaces().Where(t => t.Name.EndsWith("Factory"))
+                    .BindToFactory());
 
-            this.Kernel.Bind<IView>().To<LogView>().WhenInjectedInto<LogPresenter>();
-            this.Kernel.Bind<IView>().To<GuiView>().WhenInjectedInto<GuiPresenter>();
+            this.Kernel.Bind(
+                x =>
+                x.FromThisAssembly().SelectAllClasses().Where(t => t.Name.EndsWith("Engine"))
+                    .BindAllInterfaces()
+                    .Configure(b => b.InTransientScope())
+                    .ConfigureFor<BattleEngine>(b => b.InSingletonScope()
+                    .WithConstructorArgument("redArmySize", RedArmySize)
+                    .WithConstructorArgument("blueArmySize", BlueArmySize)));
 
-            this.Kernel.Bind<IGuiPresenter>().To<GuiPresenter>()
-                .OnActivation(p => p.Initialize());
+            this.Kernel.Bind(
+               x =>
+               x.FromThisAssembly().SelectAllClasses().InheritedFrom<IView>()
+                   .BindAllInterfaces()
+                   .Configure((b, c) => b.When(r => r.Target.Member.DeclaringType.Name == c.Name.Replace("View", "Presenter"))));
 
-            this.Kernel.Bind<ILogPresenter>().To<LogPresenter>()
-                .OnActivation(p => p.Initialize());
+            this.Kernel.Bind(
+                x =>
+                x.FromThisAssembly().SelectAllClasses().InheritedFrom<IPresenter>()
+                    .BindAllInterfaces()
+                    .Configure(b => b.OnActivation<IPresenter>(p => p.Initialize())));
 
             this.Kernel.Bind<BattleApplication>().ToSelf();
         }
