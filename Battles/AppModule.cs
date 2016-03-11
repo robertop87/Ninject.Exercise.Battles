bdda6b090d4c7a6845cc8600664781cfb5820a42
Luis Roberto Pérez Rios <roberto.perez@swissbytes.ch>
2016-03-11 14:50:35 -0400

    Task 2: DI with Ninject (part I) - Add AppModule to resolve dependencies

---
+++ Battles/AppModule.cs
@@ -0,0 +1,29 @@
+﻿using Battles.Log;
+using Battles.Misc;
+using Battles.UI;
+using Ninject.Modules;
+
+namespace Battles
+{
+    class AppModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IFightEngine>().To<FightEngine>();
+            Bind<IBattleEngine>().To<BattleEngine>()
+                .WithConstructorArgument("redArmySize", Constants.RedArmySize)
+                .WithConstructorArgument("blueArmySize", Constants.BlueArmySize);
+
+            Bind<BattleApplication>().ToSelf();
+            Bind<WarriorFactory>().ToSelf().InSingletonScope();
+
+            Bind<IView>().To<LogView>().WhenInjectedInto<LogPresenter>();
+            Bind<IView>().To<GuiView>().WhenInjectedInto<GuiPresenter>();
+
+            Bind<ILogPresenter>().To<LogPresenter>().OnActivation(p => p.Initialize());
+            Bind<IGuiPresenter>().To<GuiPresenter>().OnActivation(p => p.Initialize());
+
+            Bind<ILogService>().To<LogService>().InSingletonScope();
+        }
+    }
+}
